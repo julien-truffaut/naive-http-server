7a16fcbd52766004540aeb81c86b092c9ba4dcfa
Tim Tennant <ttennant@gmail.com>
2014-08-06 09:45:12 +0100

    Return NOT_FOUND when static resource does not exist

--- src/main/scala/io/shaka/http/StaticResponse.scala
+++ src/main/scala/io/shaka/http/StaticResponse.scala
@@ -1,14 +1,23 @@
 package io.shaka.http
 
+import java.io.FileNotFoundException
+
 import io.shaka.http.ContentType._
 import io.shaka.http.Response._
+import io.shaka.http.Status.NOT_FOUND
 
 import scala.io.Source._
 
 object StaticResponse{
-  def static(docRoot: String, path: String) = respond(path)
-    .entity(fromFile(s"$docRoot$path").mkString)
-    .contentType(TEXT_HTML)
+  def static(docRoot: String, path: String) = {
+    try {
+      respond(fromFile(s"$docRoot$path").mkString).contentType(toContentType(path))
+    }
+    catch {
+      case _:FileNotFoundException =>
+        respond(path).status(NOT_FOUND)
+    }
+  }
 
   private val fileExtensionToContentType = Map(
     "css"-> TEXT_CSS,
--- src/test/scala/io/shaka/http/ServingStaticResourcesSpec.scala
+++ src/test/scala/io/shaka/http/ServingStaticResourcesSpec.scala
@@ -1,9 +1,11 @@
 package io.shaka.http
 
+import io.shaka.http.ContentType.{TEXT_CSS, TEXT_HTML}
 import io.shaka.http.Http._
 import io.shaka.http.HttpServerSpecSupport.withHttpServer
 import io.shaka.http.Request.GET
 import io.shaka.http.StaticResponse.static
+import io.shaka.http.Status.NOT_FOUND
 import org.scalatest.Spec
 
 import scala.io.Source._
@@ -20,11 +22,31 @@ class ServingStaticResourcesSpec extends Spec{
     }
   }
 
-  def `correctly set content-type when serving static files`() {
+  def `return NOT_FOUND when static resource does not exist`(){
+    withHttpServer{ (httpServer, rootUrl) =>
+      httpServer.handler{
+        case GET(path) => static(docRoot, path)
+      }
+      assert(http(GET(s"$rootUrl/io/shaka/http/test2.html")).status === NOT_FOUND)
+    }
 
   }
 
+  def `correctly set content-type when serving static files`() {
+    withHttpServer{ (httpServer, rootUrl) =>
+      httpServer.handler{
+        case GET(path) => static(docRoot, path)
+      }
+      assert(http(GET(s"$rootUrl/io/shaka/http/test.html")).contentType.get === TEXT_HTML)
+      assert(http(GET(s"$rootUrl/io/shaka/http/test.css")).contentType.get === TEXT_CSS)
+    }
+  }
+
   def `can server a static file from the class path`() {
+
+  }
+
+  def `can server a static file from a jar`() {
 
   }
 
