43659262f819d2860689c3839b176cc9826aef8c
Tim Tennant <ttennant@gmail.com>
2014-08-07 18:43:37 +0100

    Add support for getting static resource from classpath (from file not jar - that's for later)

--- src/main/scala/io/shaka/http/StaticResponse.scala
+++ src/main/scala/io/shaka/http/StaticResponse.scala
@@ -1,6 +1,7 @@
 package io.shaka.http
 
 import java.io.File
+import java.net.URL
 
 import io.shaka.http.ContentType._
 import io.shaka.http.Response._
@@ -9,14 +10,26 @@ import io.shaka.http.Status.NOT_FOUND
 import scala.io.Source._
 
 object StaticResponse {
-  def static(docRoot: String, path: String) = new File(s"$docRoot$path") match {
+  type ClasspathDocRoot = Unit => String
+
+  def static(docRoot: String, path: String): Response = new File(s"$docRoot$path") match {
     case dir if dir.isDirectory => respond(dir.listFiles().map(_.getName).mkString("\n")).contentType(TEXT_PLAIN)
     case file if file.exists() => respond(fromFile(file.getAbsolutePath).mkString).contentType(toContentType(path))
     case _ => respond(s"file $path not found").status(NOT_FOUND)
   }
 
+  def classpathDocRoot(docRoot: String): ClasspathDocRoot = unit => docRoot
+
+  def static(docRoot: ClasspathDocRoot, path: String): Response = static(this.getClass.getClassLoader.getResource(docRoot()), path)
+
+  def static(docRoot: URL, path: String): Response = docRoot.getProtocol match {
+    case "file" => static(docRoot.getPath, path)
+    case protocol => respond(s"Doesn't currently support protocol $protocol")
+  }
+
   private val fileExtensionToContentType = Map(
     "css" -> TEXT_CSS,
+    "csv" -> TEXT_CSV,
     "htm" -> TEXT_HTML,
     "html" -> TEXT_HTML,
     "xml" -> TEXT_XML,
--- src/test/scala/io/shaka/http/ServingStaticResourcesSpec.scala
+++ src/test/scala/io/shaka/http/ServingStaticResourcesSpec.scala
@@ -1,62 +1,72 @@
 package io.shaka.http
 
-import io.shaka.http.ContentType.{TEXT_CSS, TEXT_HTML}
+import io.shaka.http.ContentType.{TEXT_CSS, TEXT_CSV, TEXT_HTML}
 import io.shaka.http.Http._
 import io.shaka.http.HttpServerSpecSupport.withHttpServer
 import io.shaka.http.Request.GET
-import io.shaka.http.StaticResponse.static
+import io.shaka.http.StaticResponse.{classpathDocRoot, static}
 import io.shaka.http.Status.NOT_FOUND
 import org.scalatest.Spec
 
 import scala.io.Source._
 
-class ServingStaticResourcesSpec extends Spec{
-  val docRoot = "./src/test/scala"
+class ServingStaticResourcesSpec extends Spec {
+  val docRoot = "./src/test/resources/web"
+
   def `can serve a static file from filesystem`() {
     withHttpServer { (httpServer, rootUrl) =>
       httpServer.handler {
         case GET(path) => static(docRoot, path)
       }
-      val response = http(GET(s"$rootUrl/io/shaka/http/test.html"))
-      assert(response.entityAsString === fromFile(s"$docRoot/io/shaka/http/test.html").mkString)
+      val response = http(GET(s"$rootUrl/test.html"))
+      assert(response.entityAsString === fromFile(s"$docRoot/test.html").mkString)
     }
   }
 
-  def `return NOT_FOUND when static resource does not exist`(){
-    withHttpServer{ (httpServer, rootUrl) =>
-      httpServer.handler{
+  def `return NOT_FOUND when static resource does not exist`() {
+    withHttpServer { (httpServer, rootUrl) =>
+      httpServer.handler {
         case GET(path) => static(docRoot, path)
       }
-      assert(http(GET(s"$rootUrl/io/shaka/http/test2.html")).status === NOT_FOUND)
+      assert(http(GET(s"$rootUrl/test2.html")).status === NOT_FOUND)
     }
 
   }
 
   def `correctly set content-type when serving static files`() {
-    withHttpServer{ (httpServer, rootUrl) =>
-      httpServer.handler{
+    withHttpServer { (httpServer, rootUrl) =>
+      httpServer.handler {
         case GET(path) => static(docRoot, path)
       }
-      assert(http(GET(s"$rootUrl/io/shaka/http/test.html")).contentType.get === TEXT_HTML)
-      assert(http(GET(s"$rootUrl/io/shaka/http/test.css")).contentType.get === TEXT_CSS)
+      assert(http(GET(s"$rootUrl/test.html")).contentType.get === TEXT_HTML)
+      assert(http(GET(s"$rootUrl/test.css")).contentType.get === TEXT_CSS)
+      assert(http(GET(s"$rootUrl/testdir/test.csv")).contentType.get === TEXT_CSV)
     }
   }
 
-  def `can server a static file from the class path`() {
+  def `shows directory listing when serving static resources`() {
+    withHttpServer { (httpServer, rootUrl) =>
+      httpServer.handler {
+        case GET(path) => static(docRoot, path)
+      }
+      val response = http(GET(s"$rootUrl/testdir"))
+      assert(response.entityAsString.split("\n").sorted === List("test.csv", "testsubdir", "test.txt").sorted)
+    }
+  }
 
+
+  def `can server a static file from the class path`() {
+    withHttpServer { (httpServer, rootUrl) =>
+      httpServer.handler {
+        case GET(path) => static(classpathDocRoot("./web"), path)
+      }
+      val response = http(GET(s"$rootUrl/test.html"))
+      assert(response.entityAsString === fromFile(s"$docRoot/test.html").mkString)
+    }
   }
 
   def `can server a static file from a jar`() {
 
   }
 
-  def `shows directory listing when serving static resources`() {
-    withHttpServer{ (httpServer, rootUrl) =>
-      httpServer.handler{
-        case GET(path) => static(docRoot, path)
-      }
-      val response = http(GET(s"$rootUrl/io/shaka/http/testdir"))
-      assert(response.entityAsString.split("\n").sorted === List("test.csv","testsubdir","test.txt").sorted)
-    }
-  }
 }
