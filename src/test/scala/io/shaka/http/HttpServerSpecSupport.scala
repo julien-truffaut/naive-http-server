1953b3800e1de51f868cd4e9c99c02af24742387
jcaraballo <jcaraballo@users.noreply.github.com>
2015-06-09 11:38:06 +0100

    Fix parentheses
    As they've been removed in naive-http-client

--- src/test/scala/io/shaka/http/HttpServerSpecSupport.scala
+++ src/test/scala/io/shaka/http/HttpServerSpecSupport.scala
@@ -4,7 +4,7 @@ object HttpServerSpecSupport {
   type RootUrl = String
   def withHttpServer(testBlock: (HttpServer, RootUrl) => Unit) {
     val httpServer = HttpServer().start()
-    testBlock(httpServer, s"http://localhost:${httpServer.port()}")
+    testBlock(httpServer, s"http://localhost:${httpServer.port}")
     httpServer.stop()
   }
 }
